90eade272e3cd738e94633a826fa93f0223b109d
KDotIV <kylebsmall32@gmail.com>
2021-02-08 16:15:30 -0500

    GET and POST
    + Get Forms
    + Create Forms

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,42 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello world");
+
+const express = require('express');
+const app = express();
+
+app.get('/forms', (req, res) => {
+    admin.firestore().collection('forms')
+    .get()
+    .then(data => {
+        let forms = [];
+        data.forEach(doc => {
+            forms.push(doc.data());
+        });
+        return res.json(forms);
+    })
+    .catch(err => console.error(err));
+})
+
+app.post('/forms', (req, res) => {
+    const newForm = {
+        body: req.body.body,
+        formHandle: req.body.formHandle
+    };
+
+    admin.firestore()
+    .collection('forms')
+    .add(newForm)
+    .then(doc => {
+        res.json({ message: `document ${doc.id} created successfully`})
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Failed to create form'})
+        console.error(err);
+    })
 });
 
-exports.GetForms = functions.https.onRequest((req, res) => {
+//Allows our api to persist within different endpoints using express
+//https://baseurl.com/api/*
 
-})
+exports.api = functions.https.onRequest(app);
