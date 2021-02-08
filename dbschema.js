10076dfaad5a980b65ed2f1e64e373c3a35f8ad4
KDotIV <kylebsmall32@gmail.com>
2021-02-08 17:13:12 -0500

    User Authentication
    +User can signup
    +Data is Validated
    + tokens are created and stored

---
+++ dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+    forms: [
+        {
+            formHandle: 'form',
+            body: 'this is the body of the PDF',
+            format: 'PDF'
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,47 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyBK7_Fl64MkxfTglJe40CBEYB7kX6mSswY",
+    authDomain: "forms-cb59f.firebaseapp.com",
+    projectId: "forms-cb59f",
+    storageBucket: "forms-cb59f.appspot.com",
+    messagingSenderId: "517201843104",
+    appId: "1:517201843104:web:9605f58944f60e4bc20ac5"
+}
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
+const db = admin.firestore();
+
+//GET FORMS
 app.get('/forms', (req, res) => {
-    admin.firestore().collection('forms')
+    db.collection('forms')
     .get()
     .then(data => {
         let forms = [];
         data.forEach(doc => {
-            forms.push(doc.data());
+            forms.push({
+                formID: doc.id,
+                body: doc.data().body,
+                formHandle: doc.data().formHandle
+            });
         });
         return res.json(forms);
     })
     .catch(err => console.error(err));
 })
 
-app.post('/forms', (req, res) => {
+//CREATE FORMS
+app.post('/form', (req, res) => {
     const newForm = {
         body: req.body.body,
         formHandle: req.body.formHandle
     };
 
-    admin.firestore()
-    .collection('forms')
+    db.collection('forms')
     .add(newForm)
     .then(doc => {
         res.json({ message: `document ${doc.id} created successfully`})
@@ -35,6 +50,52 @@ app.post('/forms', (req, res) => {
         res.status(500).json({ error: 'Failed to create form'})
         console.error(err);
     })
+});
+
+//Signup
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    }
+
+    // TODO: validate DATA
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then((doc) => {
+            if(doc.exists){
+                return res.status(400).json({ handle: 'this user is already taken'})
+            } else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then((data) => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then((idToken) => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAT: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use'){
+                return res.status(400).json({ email: 'Email is already in use'})
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        });
 });
 
 //Allows our api to persist within different endpoints using express
